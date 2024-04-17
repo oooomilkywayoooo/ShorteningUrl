689050fb72884ddb4247435fa1b72ad87ec16abf
FURUKAWA <FURUKAWA@host.docker.internal>
2024-04-17 17:38:15 +0900

    画面作成（追加）

--- src/main/java/com/example/demo/controller/MenuController.java
+++ src/main/java/com/example/demo/controller/MenuController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/menu")
+@RequestMapping("/")
 public class MenuController {
 
 	@GetMapping
