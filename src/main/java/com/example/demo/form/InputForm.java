a4846ddca944b4fc018d592e369c2ac565bb832c
user <oooomilkywayoooo@gmail.com>
2024-04-21 17:12:17 +0900

    フォームの追加

---
+++ src/main/java/com/example/demo/form/InputForm.java
@@ -0,0 +1,19 @@
+package com.example.demo.form;
+
+import lombok.Data;
+
+@Data
+public class InputForm {
+	
+	private String url1;
+	private String url2;
+	private String url3;
+	private String url4;
+	private String url5;
+	
+	private String page1;
+	private String page2;
+	private String page3;
+	private String page4;
+	private String page5;
+}
