4ab30665df93f914a8dc69f540d4019ea5f90cb7
FURUKAWA <FURUKAWA@host.docker.internal>
2024-04-22 15:59:00 +0900

    テスト

--- src/main/java/com/example/demo/controller/InputController.java
+++ src/main/java/com/example/demo/controller/InputController.java
@@ -1,16 +1,35 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.demo.form.InputForm;
+import com.example.demo.service.UrlInfoService;
+
+import lombok.RequiredArgsConstructor;
+/**
+ * インプットコントローラー
+ * 
+ * @author input
+ *
+ */
 @Controller
-@RequestMapping("/input")
+@RequiredArgsConstructor
 public class InputController {
+	
+	private final UrlInfoService service;
 
-	@GetMapping
-	public String view() {
-		return "/input";
+	@GetMapping("/input")
+	public String view(Model model, InputForm form) {
+		return "input";
 	}
 	
+	@PostMapping("/input")
+	public String input(InputForm form) {
+		
+		 System.out.println(form.toString());
+		 return "output";
+	}
 }
