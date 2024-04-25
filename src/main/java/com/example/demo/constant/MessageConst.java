72c41b7119360fa8d52f142cec76238554e776cb
user <oooomilkywayoooo@gmail.com>
2024-04-25 22:18:48 +0900

    メッセージ管理追加

---
+++ src/main/java/com/example/demo/constant/MessageConst.java
@@ -0,0 +1,13 @@
+package com.example.demo.constant;
+
+/**
+ * メッセージID定数クラス
+ * 
+ * @author user
+ *
+ */
+public class MessageConst {
+
+	/** 変換前URLが空白 */
+	public static final String INPUT_NULL = "input.null";
+}
--- src/main/java/com/example/demo/controller/InputController.java
+++ src/main/java/com/example/demo/controller/InputController.java
@@ -4,11 +4,14 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.demo.common.AppCommon;
+import com.example.demo.constant.MessageConst;
 import com.example.demo.entity.UrlInfo;
 import com.example.demo.form.InputForm;
 import com.example.demo.service.InputService;
@@ -24,15 +27,35 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class InputController {
 	
+	/** 短縮URL作成画面 Service */
 	private final InputService service;
+	
+	/** メッセージソース */
+	private final MessageSource messageSource;
 	
 	Map<String, String> outputMap = new HashMap<>(); // フォームから受け取ったURLとページ名
 	ArrayList<String> checkList = new ArrayList<>(); // 要確認に表示するURL
-
+	
+	/**
+	 * 初期表示
+	 * 
+	 * @param model モデル
+	 * @param form 変換前URL ページ名
+	 * @return 変換後URL表示画面
+	 */
+	
 	@GetMapping("/input")
 	public String view(Model model, InputForm form) {
 		return "input";
 	}
+	
+	/**
+	 * 短縮URL作成
+	 * 
+	 * @param model モデル
+	 * @param form 変換前URL ページ名
+	 * @return 変換後URL表示画面
+	 */
 	
 	@PostMapping("/input")
 	public String input(Model model, InputForm form) {
@@ -46,7 +69,8 @@ public class InputController {
 		String screen = "";
 		
 		if(outputMap == null && checkList == null) {
-			model.addAttribute("errorMsg","変換するURLを入力して下さい");
+			String errorMsg = AppCommon.getMessage(messageSource, MessageConst.INPUT_NULL);
+			model.addAttribute("errorMsg",errorMsg);
 			screen = "input";
 		}else {
 			
