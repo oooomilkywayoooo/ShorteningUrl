b0d5bddeaeae7d8d5ffe15be42ad19ce63bb4547
user <oooomilkywayoooo@gmail.com>
2024-04-28 17:05:10 +0900

    コピー機能追加

--- src/main/java/com/example/demo/controller/AccessController.java
+++ src/main/java/com/example/demo/controller/AccessController.java
@@ -23,38 +23,24 @@ import lombok.RequiredArgsConstructor;
 public class AccessController {
 	private final AccessService service;
 
+	/**
+	 * 初期表示
+	 * @param model
+	 * @return アクセス確認画面
+	 */
 	@GetMapping("/access")
-	public String showRegistrationDates(Model model,
-			@RequestParam(name = "yearMonth", required = false) String yearMonth) {
-		
-		if (yearMonth == null) {
-	        // 初回遷移時に特定の年月を取得する
-	        yearMonth = "2024-04";
-	    }
+	public String showRegistrationDates(@RequestParam(name = "yearMonth", required = false) String yearMonth,
+			Model model) {
 
+		List<String> yearMonths = service.getAllYearMonths();
+		model.addAttribute("yearMonths", yearMonths);
+
+		// 選択された値を処理
 		if (yearMonth != null) {
 			List<UrlInfo> entities = service.getEntitiesByYearMonth(yearMonth);
 			model.addAttribute("entities", entities);
 		}
 
-		List<String> yearMonths = service.getAllYearMonths();
-		model.addAttribute("yearMonths", yearMonths);
-
-		//		// データベースから登録日を取得
-		//		List<Timestamp> registrationTimestamps = repository.findAll()
-		//				.stream()
-		//				.map(UrlInfo::getCreatedAt)
-		//				.collect(Collectors.toList());
-		//		
-		//		// 年月を取得
-		//		List<String> yearMonths = registrationTimestamps.stream()
-		//				.map(timestamp -> new SimpleDateFormat("yyyy-MM").format(timestamp))
-		//				.distinct()
-		//				.collect(Collectors.toList());
-		//
-		//		model.addAttribute("yearMonths", yearMonths);
-
 		return "/access";
 	}
-
 }
---
+++ src/main/resources/static/js/common.js
@@ -0,0 +1,17 @@
+/**
+ * JavaScriptを記載
+ */
+
+function copyToClipboard() {
+	// コピー対象をJavaScript上で変数として定義する
+	var copyTarget = document.getElementById("copyTarget");
+
+	// コピー対象のテキストを選択する
+	copyTarget.select();
+
+	// 選択しているテキストをクリップボードにコピーする
+	document.clipboard.writeText("Copy");
+
+	// コピーをお知らせする
+	alert("コピーできました！ : " + copyTarget.value);
+}
